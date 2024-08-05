224ed379eb53fafcea6f9c8f1c638bdb82410d02
Kateryna Semeniuk <katyasemeniuk@gmail.com>
2024-08-05 17:52:13 +0000

    add author and position fields to the About model

--- about/models.py
+++ about/models.py
@@ -5,6 +5,9 @@ class About(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
+    author = models.CharField(max_length=200, null=True)
+    position = models.CharField(max_length=200, null=True)
+    
 
     def __str__(self):
         return self.title
